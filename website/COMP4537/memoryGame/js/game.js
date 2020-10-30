e26333112be9788eecd56bc3c553f247213c3ce8
aiglesias123 <aiglesias4@my.bcit.ca>
2020-10-29 21:18:00 -0700

    new things whee

---
+++ website/COMP4537/memoryGame/js/game.js
@@ -0,0 +1,18 @@
+
+function roundStart(x, y) {
+    let container = document.createElement("div");
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
+            let box = createElement("button");
+            // button styling
+            container.appendChild(box);
+        }
+    }
+    // trigger box assignment & the rest
+}
+
+function gameStart() {
+    // stuff
+}
+
+gameStart();
