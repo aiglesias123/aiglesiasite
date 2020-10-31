1b660c758993bd6430634b9621748684f266e95c
aiglesias123 <aiglesias4@my.bcit.ca>
2020-10-31 12:29:33 -0700

    more stuff

--- website/COMP4537/memoryGame/js/game.js
+++ website/COMP4537/memoryGame/js/game.js
@@ -1,18 +1,22 @@
 
+let squares = [];
+
 function roundStart(x, y) {
     let container = document.createElement("div");
     for (let i = 0; i < x; i++) {
         for (let j = 0; j < y; j++) {
             let box = createElement("button");
+            box.id = String(i) + String(j);
             // button styling
             container.appendChild(box);
         }
     }
     // trigger box assignment & the rest
+    $("#container").add(container);
 }
 
 function gameStart() {
     // stuff
 }
 
-gameStart();
+$(gameStart());
